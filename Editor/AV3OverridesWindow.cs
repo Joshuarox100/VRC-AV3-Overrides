854a633446d5696a0a1ad11b30bb793a3ffa215a
Joshuarox100 <joshuabender10@gmail.com>
2020-08-07 03:54:13 -0500

    i forgot what I did

---
+++ Editor/AV3OverridesWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AV3OverridesWindow : EditorWindow
+{
+    private readonly AV3OverridesManager manager = new AV3OverridesManager();
+
+    [MenuItem("Window/AV3 Overrides/Convert Override")]
+    public static void ConvertOverride()
+    {
+        AV3OverridesWindow window = (AV3OverridesWindow)GetWindow(typeof(AV3OverridesWindow), false, "AV3 Overrides");
+        //window.minSize = new Vector2(375f, 525f);
+        window.wantsMouseMove = true;
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Box("Test");
+    }
+}
